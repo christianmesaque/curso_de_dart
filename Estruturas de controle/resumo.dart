12ba9cf25a66848c0f2579785a8c39c6935486db
Christian Mesaque <you@example.com>
2022-07-12 14:01:33 -0300

    update 1.0

--- Estruturas de controle/resumo.dart
+++ Estruturas de controle/resumo.dart
@@ -1,1 +1,22 @@
-//RESUMOS DAS ESTRUTURAS CONDICIONAIS E DE REPETIÇÃO
+//ESTRUTURAS DE CONDIÇÃO
+/*
+  if/else
+  O uso do if em um programa em Dart visa verificar se determinada ação é verdadeira e executar o bloco de código contido em seu escopo.
+  
+  switch/case
+  Por sua vez, o switch/case na programação, da mesma forma que o if também tem a função de verificar se determinada ação é verdadeira e executar o bloco de código contido em seu escopo, controlando assim o fluxo do programa. O comando switch compara o valor de uma variável aos valores que serão especificados nos comandos case.
+*/
+
+//ESTRUTURAS DE REPETIÇÃO
+/*
+  for
+  A estrutura de repetição for executará um determinado bloco de código por um número definido de vezes. Esta estrutura é muito útil quando já sabemos a quantidade de vezes que precisamos executar determinado bloco de código.
+
+  while
+  O while é uma estrutura de repetição que permite executar um determinado bloco de código enquanto uma condição for verdadeira. É muito similar ao if, com a diferença que o bloco será executado enquanto a condição for verdadeira, e não se a condição for verdadeira.
+
+  Para isso, após o comando while precisamos definir uma condição que será testada a cada execução do seu loop. O while só será finalizado quando essa condição não for mais atendida.
+
+  do-while
+  O do/while é uma estrutura de repetição que terá quase o mesmo funcionamento que a estrutura de repetição while, porém, a diferença entre eles é que no do/while o comando será executado ao menos uma única vez.
+*/
