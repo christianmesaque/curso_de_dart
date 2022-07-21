be128de7db305979ae65a8e7760346abb5077720
Christian Mesaque <christianmacedo110@gmail.com>
2022-07-21 16:10:58 -0300

    update

---
+++ funções/retornar_funçao.dart
@@ -0,0 +1,39 @@
+/*funçao(p1, p2) {
+  10s -> p1
+  sentença #01
+  sentença #02
+  sentença #03
+
+  1s -> p2 
+  sentença #04
+  sentença #05
+}
+
+  funçao(3, 17) //11s
+  funçao(3, 39) //11s
+  funçao(3, 92) //11s
+
+  total: 33s
+*/
+
+/* 
+funçaoFora(p1) {
+  //10s -> p1
+  sentença #01
+  sentença #02
+  sentença #03
+
+  return (p2) {
+    //1s -> p2
+    sentença #04
+    sentença #05
+  }
+}
+
+var funçaoForaComParam3 = funçaoFora(3) //10s
+funçaoForaComParam3(17) //11s
+funçaoForaComParam3(39) //11s
+funçaoForaComParam3(92) //11s
+
+total: 13s
+*/
